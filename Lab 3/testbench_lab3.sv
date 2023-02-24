128333b2b6cfb3684ce7e5fededb0620aad28698
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-24 18:41:44 -0500

    Added Lab 3 src code

---
+++ Lab 3/testbench_lab3.sv
@@ -0,0 +1,40 @@
+`timescale 1ns/10ps
+
+module testbench_lab3 ();
+
+    logic [2:0] a, b, s;
+    logic co;
+
+    rc_adder4 UUT (
+	a,
+	b,
+	s,
+	co
+    );
+
+    initial begin
+        a = 0;
+        forever begin
+            #10 a++;
+        end
+    end
+
+    initial begin
+	b = 0;
+	forever begin
+	    #20 b++;
+	end
+    end
+
+    initial begin
+        $display("TIME | A B | S CO");   
+        $display("-----------------");    
+        $monitor("  %2d | %d %d | %d %b", 
+            $time, a, b, s, co);         
+
+        #160;
+        $finish();
+    end
+
+endmodule
+
