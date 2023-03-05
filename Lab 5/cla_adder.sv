a117cc10230ce36427cc70873ac950b31da38fd1
Rohan Singh <rohan.b.singh54@gmail.com>
2023-03-05 17:38:05 -0500

    Added Code lol

---
+++ Lab 5/cla_adder.sv
@@ -0,0 +1,19 @@
+module cla_adder #(
+parameter N = 8
+) (
+input logic [N-1:0] a, [N-1:0] b, c_in,
+output logic [N-1:0] s, c_out
+);
+logic [N-1:0] p, g;
+logic [N:0]   c;
+assign p = a ^ b;
+assign g =  a & b;
+for (genvar i = 0; i <= N; i++) begin
+if (i == 0)
+assign c[i] = c_in;
+else
+assign c[i] = g[i-1] | (p[i-1] & c[i-1]);
+end
+assign s = p ^ c;
+assign c_out = c[N];
+endmodule
